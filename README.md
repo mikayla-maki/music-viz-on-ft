c0398cac854d667a2800e8463e360ad70a87e834
Mikayla Maki <mikayla.c.maki@gmail.com>
2022-10-29 11:58:33 -0700

    Added the ability to set colors

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple program to read the computer's active audio inputs,
+generate cool looking images for the big Flaschen Taschen install at NB
+And then blast them over the FT protocol
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@ use std::io::{stdout, Write};
 const PPM_VERSION: &'static str = "P6";
 const COLOR_SPACE: u8 = u8::MAX;
 
-struct PPMP6 {
-    height: u8,
-    width: u8,
+struct PPMP6<const R: usize, const C: usize> {
+    pixels: [[Pixel; C]; R],
 }
 
-impl PPMP6 {
-    fn new(width: u8, height: u8) -> PPMP6 {
-        PPMP6 { height, width }
+#[derive(Default, Clone, Copy)]
+struct Pixel {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl<const R: usize, const C: usize> PPMP6<R, C> {
+    fn new() -> PPMP6<R, C> {
+        PPMP6 {
+            pixels: [[Pixel::default(); C]; R],
+        }
     }
 
     fn write_headers<W>(&self, writer: &mut W) -> Result<()>
@@ -19,33 +27,39 @@ impl PPMP6 {
         W: Write,
     {
         writeln!(writer, "{}", PPM_VERSION)?;
-        writeln!(writer, "{} {}", self.width, self.height)?;
+        writeln!(writer, "{} {}", C, R)?;
         writeln!(writer, "#FT 0 0 10")?;
         writeln!(writer, "{}", COLOR_SPACE)?;
 
         Ok(())
     }
 
-    fn write<W>(&self, writer: &mut W) -> Result<()>
+    fn _write<W>(&self, writer: &mut W) -> Result<()>
     where
         W: Write,
     {
         self.write_headers(writer)?;
 
-        for _row in 0..self.height {
-            for _col in 0..self.width {
-                let color = u8::MAX / 2;
-                writer.write(&[color, color, color])?;
+        for row in self.pixels {
+            for pixel in row {
+                writer.write(&[pixel.r, pixel.g, pixel.b])?;
             }
         }
         Ok(())
     }
 
+    fn set_color(&mut self, p: &Pixel) {
+        for row in self.pixels.iter_mut() {
+            for pixel in row.iter_mut() {
+                *pixel = *p;
+            }
+        }
+    }
+
     fn print_ascii(&self) {
-        for _row in 0..self.height {
-            for _col in 0..self.width {
-                let color = u8::MAX / 2;
-                print!("\x1b[38;2;{};{};{}m█\x1b[0m", color, color + 20, color - 20);
+        for row in self.pixels {
+            for pixel in row {
+                print!("\x1b[38;2;{};{};{}m█\x1b[0m", pixel.r, pixel.g, pixel.b);
             }
             println!();
         }
@@ -54,21 +68,39 @@ impl PPMP6 {
 
 fn main() -> Result<()> {
     // FT is 30 x 40 pixels in size
-    let back_buffer = PPMP6::new(30, 15);
+    let mut back_buffer = PPMP6::<15, 30>::new();
+    let mut front_buffer = PPMP6::<15, 30>::new();
+
+    back_buffer.set_color(&Pixel {
+        r: 10,
+        g: 80,
+        b: 250,
+    });
+
+    front_buffer.set_color(&Pixel {
+        r: 250,
+        g: 80,
+        b: 10,
+    });
+
+    println!("Back buffer: ");
+    back_buffer.write_headers(&mut stdout())?;
+    back_buffer.print_ascii();
+
+    println!("Front buffer: ");
+    front_buffer.write_headers(&mut stdout())?;
+    front_buffer.print_ascii();
 
     // let udp_socket = UdpSocket::bind("0.0.0.0:0")?;
 
     // let mut buf = Vec::new();
 
-    back_buffer.write_headers(&mut stdout())?;
     // back_buffer.write(&mut buf)?;
 
     // loop {
     // udp_socket.send_to(&buf, "ft.noise:1337")?;
     // sleep(Duration::from_millis(1));
     // }
-
-    back_buffer.print_ascii();
 
     Ok(())
 }
