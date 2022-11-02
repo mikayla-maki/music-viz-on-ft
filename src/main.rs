596f259371264f3427b896e8650b7a71c7bab075
Mikayla Maki <mikayla.c.maki@gmail.com>
2022-11-02 16:38:52 -0700

    Getting ready for the show

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod ppmp6;
 
 use std::{
     io::{stdin, stdout, Write},
+    net::UdpSocket,
     thread::sleep,
     time::{Duration, Instant},
 };
@@ -16,7 +17,9 @@ use cpal::{
 };
 
 use spectrum_analyzer::{
-    samples_fft_to_spectrum, scaling::scale_20_times_log10, windows::hann_window,
+    samples_fft_to_spectrum,
+    scaling::{divide_by_N, scale_20_times_log10, scale_to_zero_to_one},
+    windows::hann_window,
 };
 
 const ROWS: usize = 35;
@@ -24,8 +27,8 @@ const COLS: usize = 45;
 
 fn main() -> Result<()> {
     // FT is 30 x 40 pixels in size
-    let back_buffer = PPMP6::<ROWS, COLS>::new(Pixel::new(10, 80, 250));
-    let front_buffer = PPMP6::<ROWS, COLS>::new(Pixel::new(250, 80, 10));
+    let back_buffer = PPMP6::<ROWS, COLS>::new(Pixel::black(), 15);
+    let front_buffer = PPMP6::<ROWS, COLS>::new(Pixel::new(250, 80, 10), 14);
 
     println!("Back buffer: \n{}", back_buffer);
 
@@ -61,7 +64,9 @@ fn main() -> Result<()> {
     let sample_rate = supported_config.sample_rate();
     let config = supported_config.config();
 
-    let stream = device.build_input_stream(&config, print_stuff(sample_rate), err_fn)?;
+    let socket = UdpSocket::bind("0.0.0.0:0")?;
+
+    let stream = device.build_input_stream(&config, print_stuff(sample_rate, socket), err_fn)?;
 
     stream.play()?;
 
@@ -70,14 +75,22 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn print_stuff(samples_per_sec: SampleRate) -> impl FnMut(&[f32], &InputCallbackInfo) {
+fn print_stuff(
+    samples_per_sec: SampleRate,
+    socket: UdpSocket,
+) -> impl FnMut(&[f32], &InputCallbackInfo) {
     let samples_rate = samples_per_sec.0;
 
     let start = Instant::now();
 
+    let back_frame = PPMP6::<ROWS, COLS>::new(Pixel::black(), 10);
+    send_to_big_ft(&socket, &back_frame);
+
+    let mut frame = PPMP6::<ROWS, COLS>::new(Pixel::new(0, 0, 0), 26);
+
     move |data, _info| {
         let now = Instant::now();
-        let color_idx = now.duration_since(start).as_secs_f32() * 10.; //Speed up the cycle a lil
+        let color_idx = now.duration_since(start).as_secs_f32() * 20.; //Speed up the cycle a lil
 
         let hann_window = hann_window(data);
 
@@ -89,10 +102,9 @@ fn print_stuff(samples_per_sec: SampleRate) -> impl FnMut(&[f32], &InputCallback
         )
         .unwrap();
 
-        let mut data = PPMP6::<ROWS, COLS>::new(Pixel::new(0, 0, 0));
-
         let freq_data = spectrum_hann_window.data();
         let chunk_size = freq_data.len() / COLS;
+        let color = color_gradient(color_idx);
 
         for (idx, chunk) in freq_data.chunks(chunk_size).enumerate() {
             // let first_freq = chunk.first().map(|f| f.0);
@@ -105,15 +117,25 @@ fn print_stuff(samples_per_sec: SampleRate) -> impl FnMut(&[f32], &InputCallback
 
             if let Some(avg) = avg {
                 let avg = avg / -100.;
-                data.set_col(idx, avg, color_gradient(color_idx));
+                frame.set_col(idx, avg, color);
             }
         }
-        print!("\x1b[2J\x1b[H{}", data);
-        sleep(Duration::from_millis(50));
 
-        println!();
-        println!();
+        print!("\x1b[2J\x1b[H{}", frame);
+        println!("sent frame with {:?}", color);
+        // send_to_big_ft(&socket, &back_frame);
+        send_to_big_ft(&socket, &frame);
+        sleep(Duration::from_millis(10));
     }
+}
+
+fn send_to_big_ft<const R: usize, const C: usize>(socket: &UdpSocket, frame: &PPMP6<R, C>) {
+    let mut buf = vec![];
+    frame.write(&mut buf).ok();
+    socket
+        .send_to(&buf, "ft.noise:1337")
+        .map_err(|err| dbg!(err))
+        .ok();
 }
 
 fn color_gradient(idx: f32) -> Pixel {
--- src/ppmp6.rs
+++ src/ppmp6.rs
@@ -5,10 +5,11 @@ const PPM_VERSION: &'static str = "P6";
 const COLOR_SPACE: u8 = u8::MAX;
 
 pub struct PPMP6<const R: usize, const C: usize> {
+    layer: u8,
     pixels: [[Pixel; C]; R],
 }
 
-#[derive(Default, Clone, Copy, PartialEq, Eq)]
+#[derive(Default, Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Pixel {
     pub r: u8,
     pub g: u8,
@@ -19,22 +20,27 @@ impl Pixel {
     pub fn new(r: u8, g: u8, b: u8) -> Pixel {
         Pixel { r, g, b }
     }
+
+    pub fn black() -> Pixel {
+        Pixel { r: 1, g: 1, b: 1 }
+    }
 }
 
 impl<const R: usize, const C: usize> PPMP6<R, C> {
-    pub fn new(p: Pixel) -> PPMP6<R, C> {
+    pub fn new(p: Pixel, layer: u8) -> PPMP6<R, C> {
         PPMP6 {
+            layer,
             pixels: [[p; C]; R],
         }
     }
 
-    fn _write_headers<W>(&self, writer: &mut W) -> Result<()>
+    fn write_headers<W>(&self, writer: &mut W) -> Result<()>
     where
         W: Write,
     {
         writeln!(writer, "{}", PPM_VERSION)?;
         writeln!(writer, "{} {}", C, R)?;
-        writeln!(writer, "#FT 0 0 100")?;
+        writeln!(writer, "#FT: 0 0 {}", self.layer)?;
         writeln!(writer, "{}", COLOR_SPACE)?;
 
         Ok(())
@@ -46,17 +52,17 @@ impl<const R: usize, const C: usize> PPMP6<R, C> {
     {
         writeln!(writer, "{}", PPM_VERSION)?;
         writeln!(writer, "{} {}", C, R)?;
-        writeln!(writer, "#FT 0 0 100")?;
+        writeln!(writer, "#FT: 0 0 {}", self.layer)?;
         writeln!(writer, "{}", COLOR_SPACE)?;
 
         Ok(())
     }
 
-    pub fn _write<W>(&self, writer: &mut W) -> Result<()>
+    pub fn write<W>(&self, writer: &mut W) -> Result<()>
     where
         W: Write,
     {
-        self._write_headers(writer)?;
+        self.write_headers(writer)?;
 
         for row in self.pixels {
             for pixel in row {
@@ -77,10 +83,16 @@ impl<const R: usize, const C: usize> PPMP6<R, C> {
         }
         assert!(0. <= height && height <= 1.);
 
+        let height = height / 3.0;
+
         let row = (height * R as f32).round() as usize;
 
-        for i in 0..(R - row) {
-            self.pixels[R - i - 1][col_index] = p;
+        for i in 0..R {
+            if i < R - row {
+                self.pixels[R - i - 1][col_index] = p;
+            } else {
+                self.pixels[R - i - 1][col_index] = Pixel::black();
+            }
         }
     }
 }
